681777daa3c25486b47ef566a1bd8176a84b81ea
florimondmanca <florimond.manca@gmail.com>
2018-10-07 20:43:22 +0200

    update deploy ssh keys

--- scripts/before_deploy.sh
+++ scripts/before_deploy.sh
@@ -1,5 +1,5 @@
 echo "Setting up deploy SSH keys…"
-openssl aes-256-cbc -K $encrypted_0d682b85e21c_key -iv $encrypted_0d682b85e21c_iv -in deploy_rsa.enc -out /tmp/deploy_rsa -d
+openssl aes-256-cbc -K $encrypted_e4f690c90820_key -iv $encrypted_e4f690c90820_iv -in deploy_rsa.enc -out deploy_rsa -d
 eval "$(ssh-agent -s)"
 chmod 600 /tmp/deploy_rsa
 ssh-add /tmp/deploy_rsa
