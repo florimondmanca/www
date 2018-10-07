c5ff0b97de6805dd3be94431f93599ecd68d118a
florimondmanca <florimond.manca@gmail.com>
2018-10-07 15:46:22 +0200

    Add dev and prod servers

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 # Portfolio
 
-This is my personal portfolio website. For now, it's pretty basic.
+This is my personal portfolio website. For now, it's pretty basic. Soon with lots of useful information and fancy animations.
+
+## Install
+
+Just like your usual Node project:
+
+```bash
+$ npm install
+```
+
+## Quickstart
+
+Run the hot-reloaded development server:
+
+```bash
+$ npm run dev
+```
+
+Run the production server:
+
+```bash
+$ npm start
+```
---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+
+const app = express();
+app.use(morgan('combined'));
+
+const PORT = process.env.PORT || 4200;
+const HOST = process.env.HOST || 'localhost';
+const PUBLIC_FOLDER = path.join(__dirname, 'public');
+
+// Serve static files from /public
+app.get('*.*', express.static(PUBLIC_FOLDER));
+
+// All other routes go to index.html
+app.get('*', function(req, res) {
+  res.sendFile(path.join(PUBLIC_FOLDER, 'index.html'));
+});
+
+// Start up the Node server
+app.listen(PORT, HOST, () => {
+  console.log(`Server listening on http://${HOST}:${PORT}`);
+});
