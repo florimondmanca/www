4158f5f1ce22fec935fec88a4fdd182eca003bf0
florimondmanca <florimond.manca@gmail.com>
2019-03-03 11:25:42 +0100

    bind to 0.0.0.0 host by default

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-const express = require('express');
-const path = require('path');
-const morgan = require('morgan');
+const express = require("express");
+const path = require("path");
+const morgan = require("morgan");
 
 const app = express();
-app.use(morgan('combined'));
+app.use(morgan("combined"));
 
 const PORT = process.env.PORT || 4200;
-const HOST = process.env.HOST || 'localhost';
-const PUBLIC_FOLDER = path.join(__dirname, 'public');
+const HOST = process.env.HOST || "0.0.0.0";
+const PUBLIC_FOLDER = path.join(__dirname, "public");
 
 // Serve static files from /public
-app.get('*.*', express.static(PUBLIC_FOLDER));
+app.get("*.*", express.static(PUBLIC_FOLDER));
 
 // All other routes go to index.html
-app.get('*', function(req, res) {
-  res.sendFile(path.join(PUBLIC_FOLDER, 'index.html'));
+app.get("*", function(req, res) {
+  res.sendFile(path.join(PUBLIC_FOLDER, "index.html"));
 });
 
 // Start up the Node server
