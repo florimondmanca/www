2101ea1e3f872cbf057741c472fdf04773c3824d
florimondmanca <florimond.manca@gmail.com>
2019-03-03 10:48:56 +0100

    remove captainduckduck and deploy config

--- scripts/before_deploy.sh
+++
@@ -1,8 +0,0 @@
-echo "Setting up deploy SSH keys…"
-openssl aes-256-cbc -K $encrypted_e4f690c90820_key -iv $encrypted_e4f690c90820_iv -in deploy_rsa.enc -out /tmp/deploy_rsa -d
-eval "$(ssh-agent -s)"
-chmod 600 /tmp/deploy_rsa
-ssh-add /tmp/deploy_rsa
-
-echo "Installing CaptainDuckDuck CLI…"
-npm install -g captainduckduck
--- scripts/deploy.sh
+++
@@ -1,5 +0,0 @@
-echo "Copying public/ folder to Captain shared Nginx folder on remote…"
-rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/public root@captain.florimondmanca.com:/captain/nginx-shared/portfolio
-
-echo "Deploying using CaptainDuckDuck CLI…"
-captainduckduck deploy -s -h https://captain.florimondmanca.com -a www -p $CAPTAIN_PASSWORD
