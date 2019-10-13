4c45a8cf60e2d8d1b18d18140bf2f47caa5a2fb7
florimondmanca <florimond.manca@gmail.com>
2019-10-13 13:42:44 +0200

    Setup error pages

--- app/app.py
+++ app/app.py
@@ -15,3 +15,26 @@ async def dashboard(request):
     template = "index.html.jinja"
     context = {"request": request}
     return templates.TemplateResponse(template, context)
+
+
+@app.route("/500")
+async def error(request):
+    """An example error.
+    
+    Switch the `debug` setting to see either tracebacks or 500 pages.
+    """
+    raise RuntimeError("Oh no")
+
+
+@app.exception_handler(404)
+async def not_found(request, exc):
+    template = "404.html.jinja"
+    context = {"request": request}
+    return templates.TemplateResponse(template, context, status_code=404)
+
+
+@app.exception_handler(500)
+async def server_error(request, exc):
+    template = "500.html.jinja"
+    context = {"request": request}
+    return templates.TemplateResponse(template, context, status_code=500)
