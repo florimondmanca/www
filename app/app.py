61870c17888829156ef6bc37c8b6b3cd47d9d021
florimondmanca <florimond.manca@gmail.com>
2019-10-13 13:39:16 +0200

    Setup Bootstrap styling

--- app/app.py
+++ app/app.py
@@ -1,15 +1,17 @@
 from starlette.applications import Starlette
+from starlette.staticfiles import StaticFiles
 from starlette.templating import Jinja2Templates
 
 from . import settings
 
-templates = Jinja2Templates(directory="templates")
-
 app = Starlette(debug=settings.DEBUG)
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
 
 
 @app.route("/", name="home")
 async def dashboard(request):
-    template = "index.jinja"
+    template = "index.html.jinja"
     context = {"request": request}
     return templates.TemplateResponse(template, context)
