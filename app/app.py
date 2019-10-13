81672da2c5aa7c0fae82720398f21f4d386c3147
florimondmanca <florimond.manca@gmail.com>
2019-10-13 13:27:02 +0200

    Setup jinja2 templates

--- app/app.py
+++ app/app.py
@@ -1,3 +1,15 @@
 from starlette.applications import Starlette
+from starlette.templating import Jinja2Templates
 
-app = Starlette()
+from . import settings
+
+templates = Jinja2Templates(directory="templates")
+
+app = Starlette(debug=settings.DEBUG)
+
+
+@app.route("/", name="home")
+async def dashboard(request):
+    template = "index.jinja"
+    context = {"request": request}
+    return templates.TemplateResponse(template, context)
