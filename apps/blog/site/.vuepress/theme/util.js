ed67859d38fd38e4124bbf002ae515920d473664
florimondmanca <florimond.manca@gmail.com>
2019-10-26 18:55:34 +0200

    Sort posts in anti-chronological order

--- apps/blog/site/.vuepress/theme/util.js
+++ apps/blog/site/.vuepress/theme/util.js
@@ -1,6 +1,15 @@
+const sortDescBy = (array, getAttr) => {
+  return [...array].sort((a, b) => {
+    if (getAttr(a) < getAttr(b)) return 1;
+    if (getAttr(a) > getAttr(b)) return -1;
+    return 0;
+  });
+};
+
 export function getArticlePages(pages, { tag } = {}) {
-  return pages
+  const filteredPages = pages
     .filter(p => p.path.indexOf("/articles/") >= 0)
     .filter(p => p.frontmatter.published)
     .filter(p => (tag ? (p.frontmatter.tags || []).includes(tag) : true));
+  return sortDescBy(filteredPages, p => p.frontmatter.date);
 }
