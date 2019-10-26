07b659ad0678527fae159ec50097a47c905a9625
florimondmanca <florimond.manca@gmail.com>
2019-10-26 16:52:12 +0200

    Don't add subdomain redirection if blog not enabled

--- apps/web/app.py
+++ apps/web/app.py
@@ -1,8 +1,16 @@
-from apps import blog, feature_flags, index
+from starlette.types import ASGIApp
+from apps import feature_flags, index
 
 from .middleware import SubdomainRedirectMiddleware
 
-if feature_flags.BLOG_ENABLED:
-    index.app.mount("/blog", app=blog.app)
 
-app = SubdomainRedirectMiddleware(index.app, mapping={"blog": "/blog/"})
+def get_app() -> ASGIApp:
+    if feature_flags.BLOG_ENABLED:
+        from apps import blog
+
+        index.app.mount("/blog", app=blog.app)
+        return SubdomainRedirectMiddleware(index.app, mapping={"blog": "/blog/"})
+    return index.app
+
+
+app = get_app()
