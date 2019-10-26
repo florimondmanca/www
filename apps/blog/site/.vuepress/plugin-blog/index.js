2e6360ef9ae6e4846ffc70e6cff617acb0f266f3
florimondmanca <florimond.manca@gmail.com>
2019-10-26 18:06:00 +0200

    Generate tag pages from articles

--- apps/blog/site/.vuepress/plugin-blog/index.js
+++ apps/blog/site/.vuepress/plugin-blog/index.js
@@ -1,24 +1,24 @@
 const { capitalize } = require("./util");
 
 module.exports = (options, ctx) => {
-  // TODO: compute from pages.
-  const tags = ["python", "devtips"];
-
-  const additionalPages = [];
-
-  tags.forEach(tag =>
-    additionalPages.push({
-      permalink: `/t/${tag}`,
-      frontmatter: {
-        layout: "Layout",
-        title: `${capitalize(tag)} - Florimond Manca`,
-        tag
-      }
-    })
-  );
-
   return {
     name: "blog",
-    additionalPages
+    async ready() {
+      const tags = new Set();
+      ctx.pages.forEach(page => {
+        (page.frontmatter.tags || []).forEach(tag => tags.add(tag));
+      });
+
+      const additionalPages = Array.from(tags).map(tag => ({
+        permalink: `/t/${tag}`,
+        frontmatter: {
+          layout: "Layout",
+          title: `${capitalize(tag)} - Florimond Manca`,
+          tag
+        }
+      }));
+
+      await Promise.all(additionalPages.map(page => ctx.addPage(page)));
+    }
   };
 };
