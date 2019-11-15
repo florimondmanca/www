74802ef1dbdb5155759ebfb2cd4be9335ff506d2
florimondmanca <florimond.manca@gmail.com>
2019-11-15 17:11:20 +0100

    Check response content types

--- tests/test_blog.py
+++ tests/test_blog.py
@@ -5,34 +5,39 @@ def test_root(client: TestClient) -> None:
     url = "http://florimond.dev/blog/"
     resp = client.get(url, allow_redirects=False)
     assert resp.status_code == 200, resp.url
+    assert "text/html" in resp.headers["content-type"]
 
 
 def test_article_index(client: TestClient) -> None:
     url = "http://florimond.dev/blog/articles/2018/07/let-the-journey-begin/index.html"
     resp = client.get(url, allow_redirects=False)
     assert resp.status_code == 200, resp.url
+    assert "text/html" in resp.headers["content-type"]
 
 
 def test_article(client: TestClient) -> None:
     url = "http://florimond.dev/blog/articles/2018/07/let-the-journey-begin/"
     resp = client.get(url, allow_redirects=False)
     assert resp.status_code == 200, resp.url
+    assert "text/html" in resp.headers["content-type"]
 
 
 def test_article_no_slash(client: TestClient) -> None:
     url = "http://florimond.dev/blog/articles/2018/07/let-the-journey-begin"
     resp = client.get(url, allow_redirects=False)
     assert resp.status_code == 200, resp.url
+    assert "text/html" in resp.headers["content-type"]
 
 
 def test_tag(client: TestClient) -> None:
     url = "http://florimond.dev/blog/tag/python"
     resp = client.get(url, allow_redirects=False)
     assert resp.status_code == 200, resp.url
+    assert "text/html" in resp.headers["content-type"]
 
 
 def test_not_found(client: TestClient) -> None:
     url = "http://florimond.dev/blog/foo"
     resp = client.get(url)
     assert resp.status_code == 404
-    assert resp.headers["content-type"] == "text/html; charset=utf-8"
+    assert "text/html" in resp.headers["content-type"]
