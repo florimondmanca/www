b4a325121aee37f4dc325b8d69e353bc77c6dc69
Florimond Manca <florimond.manca@gmail.com>
2019-11-15 16:46:14 +0100

    Add legacy blog URLs generator (#11)
    * Add legacy blog URLs generator
    
    * Add colored logging in plugin-blog

--- src/www/blog/settings.py
+++ src/www/blog/settings.py
@@ -2,5 +2,8 @@ import pathlib
 
 HERE = pathlib.Path(__file__).parent
 BUILD_DIR = HERE / "site" / ".vuepress" / "dist"
+GENERATED_DIR = HERE / "site" / ".vuepress" / "generated"
 
-assert BUILD_DIR.exists(), "Blog site hasn't been built yet. HINT: $ scripts/build"
+assert (
+    BUILD_DIR.exists() and GENERATED_DIR.exists()
+), "Blog site hasn't been built yet. HINT: $ scripts/build"
--- src/www/blog/site/.vuepress/plugin-blog/index.js
+++ src/www/blog/site/.vuepress/plugin-blog/index.js
@@ -1,31 +1,70 @@
+const Path = require("path");
+const fs = require("fs").promises;
+const chalk = require("chalk");
+const logger = require("@vuepress/shared-utils").logger;
 const { capitalize } = require("./util");
+
+const rstrip = (str, chars) =>
+  str.endsWith(chars) ? str.slice(0, -chars.length) : str;
+
+async function generateLegacyPagesUrlMapping(ctx) {
+  const urlMapping = {};
+  ctx.pages
+    .filter(p => p.path.indexOf("/articles/") >= 0)
+    .filter(p => p.frontmatter.published)
+    .filter(page => page.frontmatter.legacy_url)
+    .forEach(page => {
+      urlMapping[page.frontmatter.legacy_url] = rstrip(page.path, "/");
+    });
+
+  const path = Path.resolve(
+    ctx.sourceDir,
+    ".vuepress",
+    "generated",
+    "legacy-blog-url-mapping.json"
+  );
+  const data = JSON.stringify(urlMapping, null, 2);
+
+  logger.info(`Write legacy blog URL mapping ${chalk.magenta(path)}`);
+  await fs.writeFile(path, data);
+}
+
+async function addTagPages(ctx) {
+  const tags = new Set();
+  ctx.pages.forEach(page => {
+    (page.frontmatter.tags || []).forEach(tag => tags.add(tag));
+  });
+
+  const additionalPages = Array.from(tags).map(tag => ({
+    path: `/tag/${tag}/`,
+    frontmatter: {
+      layout: "Layout",
+      title: `${capitalize(tag)} - Florimond Manca`,
+      tag
+    }
+  }));
+
+  await Promise.all(
+    additionalPages.map(page => {
+      logger.info(`Add tag page ${chalk.magenta(page.path)}`);
+      return ctx.addPage(page);
+    })
+  );
+}
 
 module.exports = (options, ctx) => {
   return {
     name: "blog",
 
-    extendPageData(pageCtx) {
-      if (pageCtx.regularPath && pageCtx.regularPath.startsWith("/articles/")) {
-        pageCtx.frontmatter.permalink = "/articles/:year/:month/:slug";
+    extendPageData(page) {
+      if (page.regularPath && page.regularPath.startsWith("/articles/")) {
+        page.frontmatter.permalink = "/articles/:year/:month/:slug";
       }
     },
 
     async ready() {
-      const tags = new Set();
-      ctx.pages.forEach(page => {
-        (page.frontmatter.tags || []).forEach(tag => tags.add(tag));
-      });
-
-      const additionalPages = Array.from(tags).map(tag => ({
-        path: `/tag/${tag}/`,
-        frontmatter: {
-          layout: "Layout",
-          title: `${capitalize(tag)} - Florimond Manca`,
-          tag
-        }
-      }));
-
-      await Promise.all(additionalPages.map(page => ctx.addPage(page)));
+      await generateLegacyPagesUrlMapping(ctx);
+      await addTagPages(ctx);
     }
   };
 };
