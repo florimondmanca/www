71b25d941013b6a8432de377f4476aefbfc865f3
florimondmanca <florimond.manca@gmail.com>
2019-11-15 16:34:08 +0100

    Add legacy blog URLs generator

--- src/www/blog/site/.vuepress/plugin-blog/index.js
+++ src/www/blog/site/.vuepress/plugin-blog/index.js
@@ -1,4 +1,49 @@
+const Path = require("path");
+const fs = require("fs").promises;
 const { capitalize } = require("./util");
+
+const rstrip = (str, chars) =>
+  str.endsWith(chars) ? str.slice(0, -chars.length) : str;
+
+async function generateLegacyPagesUrlMapping(ctx) {
+  const urlMapping = {};
+  ctx.pages
+    .filter(p => p.path.indexOf("/articles/") >= 0)
+    .filter(p => p.frontmatter.published)
+    .filter(page => page.frontmatter.legacy_url)
+    .forEach(page => {
+      urlMapping[page.frontmatter.legacy_url] = rstrip(page.path, "/");
+    });
+
+  const path = Path.resolve(
+    ctx.sourceDir,
+    ".vuepress",
+    "generated",
+    "legacy-blog-url-mapping.json"
+  );
+  const data = JSON.stringify(urlMapping, null, 2);
+
+  console.info("Writing legacy blog URL mapping to:", path);
+  await fs.writeFile(path, data);
+}
+
+async function addTagPages(ctx) {
+  const tags = new Set();
+  ctx.pages.forEach(page => {
+    (page.frontmatter.tags || []).forEach(tag => tags.add(tag));
+  });
+
+  const additionalPages = Array.from(tags).map(tag => ({
+    path: `/tag/${tag}/`,
+    frontmatter: {
+      layout: "Layout",
+      title: `${capitalize(tag)} - Florimond Manca`,
+      tag
+    }
+  }));
+
+  await Promise.all(additionalPages.map(page => ctx.addPage(page)));
+}
 
 module.exports = (options, ctx) => {
   return {
@@ -11,21 +56,8 @@ module.exports = (options, ctx) => {
     },
 
     async ready() {
-      const tags = new Set();
-      ctx.pages.forEach(page => {
-        (page.frontmatter.tags || []).forEach(tag => tags.add(tag));
-      });
-
-      const additionalPages = Array.from(tags).map(tag => ({
-        path: `/tag/${tag}/`,
-        frontmatter: {
-          layout: "Layout",
-          title: `${capitalize(tag)} - Florimond Manca`,
-          tag
-        }
-      }));
-
-      await Promise.all(additionalPages.map(page => ctx.addPage(page)));
+      await generateLegacyPagesUrlMapping(ctx);
+      await addTagPages(ctx);
     }
   };
 };
