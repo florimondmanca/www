277a0ac9243fdc59a6abd84686a95752754e8202
florimondmanca <florimond.manca@gmail.com>
2019-11-15 15:55:59 +0100

    Add frontmatter linter

--- src/www/blog/site/.vuepress/config.js
+++ src/www/blog/site/.vuepress/config.js
@@ -1,6 +1,53 @@
 module.exports = {
   base: "/blog/",
-  plugins: [require("./plugin-blog")],
+  plugins: [
+    require("./plugin-blog"),
+    [
+      require("vuepress-frontmatter-lint"),
+      {
+        abortBuild: true,
+        exclude: ["/", "/tag/*"],
+        specs: {
+          home: {
+            type: Boolean,
+            required: false
+          },
+          published: {
+            type: Boolean,
+            required: true
+          },
+          permalink: {
+            type: String,
+            required: false
+          },
+          title: {
+            type: String,
+            required: true
+          },
+          description: {
+            type: String,
+            required: true
+          },
+          date: {
+            type: String,
+            required: true
+          },
+          legacy_url: {
+            type: String,
+            required: false
+          },
+          tags: {
+            type: Array,
+            required: false
+          },
+          image: {
+            type: Object,
+            required: false
+          }
+        }
+      }
+    ]
+  ],
   head: [
     // Icons.
     [
