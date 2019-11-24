5551880106a97f873809edf1e775aa869e5dab5b
florimondmanca <florimond.manca@gmail.com>
2019-11-24 14:36:20 +0100

    Refactor blog app selection

--- src/www/blog/app.py
+++ src/www/blog/app.py
@@ -2,7 +2,7 @@ from starlette import status
 from starlette.datastructures import URL
 from starlette.responses import RedirectResponse
 from starlette.staticfiles import StaticFiles
-from starlette.types import Receive, Scope, Send
+from starlette.types import ASGIApp, Receive, Scope, Send
 
 from . import settings
 from .middleware import Raise404Middleware
@@ -11,6 +11,11 @@ static = Raise404Middleware(StaticFiles(directory=str(settings.BUILD_DIR)))
 
 
 async def app(scope: Scope, receive: Receive, send: Send) -> None:
+    responder = get_responder(scope)
+    await responder(scope, receive, send)
+
+
+def get_responder(scope: Scope) -> ASGIApp:
     if scope["path"] in settings.BLOG_LEGACY_URL_MAPPING:
         mapped_path = settings.BLOG_LEGACY_URL_MAPPING[scope["path"]]
         redirect_path = scope.get("root_path", "") + mapped_path
@@ -18,15 +23,14 @@ async def app(scope: Scope, receive: Receive, send: Send) -> None:
             URL(scope=scope).replace(path=redirect_path),
             status_code=status.HTTP_301_MOVED_PERMANENTLY,
         )
-        await response(scope, receive, send)
-        return
+        return response
 
     if is_page(scope["path"]):
         # Be sure to append 'index.html' as VuePress outputs pages
         # into folders containing a single 'index.html' file.
         scope["path"] = ensure_ends_with_index_html(scope["path"])
 
-    await static(scope, receive, send)
+    return static
 
 
 def is_page(path: str) -> bool:
