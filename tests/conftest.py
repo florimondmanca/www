bec1a47086a2847ec5f06c1f22e063123ab2ee2c
florimondmanca <florimond.manca@gmail.com>
2019-12-22 23:32:07 +0100

    Speedup tests using session-scoped client

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,4 @@
+import asyncio
 import typing
 
 import httpx
@@ -7,7 +8,17 @@ from asgi_lifespan.manager import LifespanManager
 import web
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
+def event_loop() -> typing.Iterator[asyncio.AbstractEventLoop]:
+    """
+    Redefine from pytest-asyncio, as the default fixture is function-scoped.
+    """
+    loop = asyncio.get_event_loop()
+    yield loop
+    loop.close()
+
+
+@pytest.fixture(scope="session")
 async def client() -> typing.AsyncIterator[httpx.AsyncClient]:
     async with LifespanManager(web.app):
         async with httpx.AsyncClient(app=web.app) as client:
