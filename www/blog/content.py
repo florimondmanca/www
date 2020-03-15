49c4ac78c7425d75043fabd978260096a414110a
florimondmanca <florimond.manca@gmail.com>
2020-03-15 16:34:03 +0100

    Lint

--- www/blog/content.py
+++ www/blog/content.py
@@ -1,7 +1,6 @@
-from .. import settings
 from .loader import load_pages
 from .resources import index
 
 
 async def load() -> None:
-    index.pages = await load_pages(root=settings.BLOG_CONTENT_DIR)
+    index.pages = await load_pages()
