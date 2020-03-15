472375887d1f89cc32668264f82b5b0b9c2b4921
florimondmanca <florimond.manca@gmail.com>
2020-03-15 20:16:42 +0100

    Refactor

--- www/blog/event_handlers.py
+++ www/blog/event_handlers.py
@@ -1,6 +1,6 @@
 from .. import resources, settings
 from . import content
-from .reloader import ContentReloader
+from .reload import PathReload
 
 on_startup = [content.load_content]
 on_shutdown = []
@@ -9,9 +9,12 @@ if settings.DEBUG:
 
     async def notify_reload() -> None:
         channel = settings.BLOG_RELOAD_CHANNEL
-        message = "reload"
-        await resources.broadcast.publish(channel, message)
+        await resources.broadcast.publish(channel, message="reload")
 
-    reloader = ContentReloader(on_reload=[content.load_content, notify_reload])
-    on_startup += [reloader.start]
-    on_shutdown += [reloader.stop]
+    reload = PathReload(
+        iter_paths=lambda: (str(path) for path in content.iter_content_paths()),
+        on_reload=[content.load_content, notify_reload],
+    )
+
+    on_startup += [reload.startup]
+    on_shutdown += [reload.shutdown]
