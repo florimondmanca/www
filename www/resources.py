a853a228e5a05a3ea2b4535933be72f7cc213057
florimondmanca <florimond.manca@gmail.com>
2020-05-23 20:06:51 +0200

    Drop monitoring

--- www/resources.py
+++ www/resources.py
@@ -1,16 +1,12 @@
 import datetime as dt
 
-import datadog
-import ddtrace
 import markdown as md
-from ddtrace.filters import FilterRequestsOnUrl
 from starlette.exceptions import HTTPException
 from starlette.staticfiles import StaticFiles
 from starlette.templating import Jinja2Templates
 
 from . import settings
 from .models import Index
-from .monitoring import FilterDropIf, FilterRedirectResponses
 from .reload import hotreload
 
 templates = Jinja2Templates(directory=str(settings.TEMPLATES_DIR))
@@ -28,16 +24,3 @@ templates.env.globals["hotreload"] = hotreload
 
 index = Index()
 markdown = md.Markdown(extensions=settings.MARKDOWN_EXTENSIONS)
-
-tracer = ddtrace.Tracer()
-trace_filters = [
-    FilterRedirectResponses(),
-    FilterRequestsOnUrl(settings.DD_TRACE_FILTER_URLS),
-    FilterDropIf(lambda: settings.TESTING),
-]
-
-statsd = datadog.DogStatsd(
-    host="dummy" if settings.TESTING else settings.DD_AGENT_HOST,
-    port=8125,
-    constant_tags=settings.DD_TAGS,
-)
