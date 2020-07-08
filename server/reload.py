883f0ce673e7011c453e7acf71d5d730dba2eee7
Florimond Manca <florimond.manca@gmail.com>
2020-07-08 17:01:02 +0200

    Use arel 0.2.0 (#76)

--- server/reload.py
+++ server/reload.py
@@ -2,4 +2,10 @@ import arel
 
 from . import content
 
-hotreload = arel.HotReload("./content", on_reload=[content.load_content])
+hotreload = arel.HotReload(
+    paths=[
+        arel.Path("./content", on_reload=[content.load_content]),
+        arel.Path("./server/templates"),
+        arel.Path("./server/static"),
+    ]
+)
