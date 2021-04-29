bdbcd920a3723c0826c80ae66f113991f8437f43
Florimond Manca <florimond.manca@gmail.com>
2021-04-29 22:38:28 +0200

    Add remaining skeleton FR translations (#209)

--- server/i18n/gettext.py
+++ server/i18n/gettext.py
@@ -8,5 +8,10 @@ def gettext(message: str) -> str:
     return locale.gettext(message)
 
 
+def ngettext(singular: str, plural: str, count: int) -> str:
+    # We don't need this yet.
+    raise NotImplementedError  # pragma: no cover
+
+
 def gettext_lazy(string: str) -> LazyProxy:
     return LazyProxy(gettext, string, enable_cache=False)
