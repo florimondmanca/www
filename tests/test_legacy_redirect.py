208d875e5b4b6301f91e303a8287a9660a231db3
Florimond Manca <florimond.manca@gmail.com>
2021-04-29 12:37:17 +0200

    Improve legacy redirect test (#200)

--- tests/test_legacy_redirect.py
+++ tests/test_legacy_redirect.py
@@ -50,123 +50,118 @@ async def test_legacy_redirect_chains(
 
 
 @pytest.mark.parametrize(
-    "blog_dot_dev_path, dot_dev_path",
+    "legacy_path, path",
     [
-        ("/let-the-journey-begin", "/articles/2018/07/let-the-journey-begin/"),
+        ("/let-the-journey-begin", "/blog/articles/2018/07/let-the-journey-begin/"),
         (
             "/a-practical-usage-of-chainmap-in-python",
-            "/articles/2018/07/a-practical-usage-of-chainmap-in-python/",
+            "/blog/articles/2018/07/a-practical-usage-of-chainmap-in-python/",
         ),
         (
             "/cheap-and-easy-deployment-captainduckduck-1",
-            "/articles/2018/07/cheap-and-easy-deployment-captainduckduck-1/",
+            "/blog/articles/2018/07/cheap-and-easy-deployment-captainduckduck-1/",
         ),
         (
             "/why-i-started-using-python-type-annotations-and-why-you-should-too",
-            (
-                "/articles/2018/07"
-                "/why-i-started-using-python-type-annotations-and-why-you-should-too/"
-            ),
+            "/blog/articles/2018/07/why-i-started-using-python-type-annotations-and-why-you-should-too/",  # noqa
         ),
         (
             "/cheap-and-easy-deployment-captainduckduck-2",
-            "/articles/2018/08/cheap-and-easy-deployment-captainduckduck-2/",
+            "/blog/articles/2018/08/cheap-and-easy-deployment-captainduckduck-2/",
         ),
         (
             "/python-mutable-defaults-are-the-source-of-all-evil",
-            "/articles/2018/08/python-mutable-defaults-are-the-source-of-all-evil/",
+            "/blog/articles/2018/08/python-mutable-defaults-are-the-source-of-all-evil/",  # noqa
         ),
         (
             "/restful-api-design-13-best-practices-to-make-your-users-happy",
-            (
-                "/articles/2018/08"
-                "/restful-api-design-13-best-practices-to-make-your-users-happy/"
-            ),
+            "/blog/articles/2018/08/restful-api-design-13-best-practices-to-make-your-users-happy/",  # noqa
         ),
         (
             "/breaking-news-everything-is-an-event",
-            "/articles/2018/09/breaking-news-everything-is-an-event/",
+            "/blog/articles/2018/09/breaking-news-everything-is-an-event/",
         ),
         (
             "/building-a-streaming-fraud-detection-system-with-kafka-and-python",
-            (
-                "/articles/2018/09"
-                "/building-a-streaming-fraud-detection-system-with-kafka-and-python/"
-            ),
+            "/blog/articles/2018/09/building-a-streaming-fraud-detection-system-with-kafka-and-python/",  # noqa
         ),
         (
             "/consuming-apis-in-angular-the-model-adapter-pattern",
-            "/articles/2018/09/consuming-apis-in-angular-the-model-adapter-pattern/",
+            "/blog/articles/2018/09/consuming-apis-in-angular-the-model-adapter-pattern/",  # noqa
         ),
         (
             "/streaming-applications-with-apache-kafka-the-opening",
-            "/articles/2018/10/streaming-applications-with-apache-kafka-the-opening/",
+            "/blog/articles/2018/10/streaming-applications-with-apache-kafka-the-opening/",  # noqa
         ),
         (
             "/from-angular-to-vue-feeling-like-a-beginner-again",
-            "/articles/2018/10/from-angular-to-vue-feeling-like-a-beginner-again/",
+            "/blog/articles/2018/10/from-angular-to-vue-feeling-like-a-beginner-again/",
         ),
         (
             "/inbox-zero-how-to-keep-clean-email-inbox-and-mind",
-            "/articles/2018/10/inbox-zero-how-to-keep-clean-email-inbox-and-mind/",
+            "/blog/articles/2018/10/inbox-zero-how-to-keep-clean-email-inbox-and-mind/",
         ),
         (
             "/reconciling-dataclasses-and-properties-in-python",
-            "/articles/2018/10/reconciling-dataclasses-and-properties-in-python/",
+            "/blog/articles/2018/10/reconciling-dataclasses-and-properties-in-python/",
         ),
         (
             "/how-i-built-a-web-framework-and-became-an-open-source-maintainer",
-            (
-                "/articles/2018/12"
-                "/how-i-built-a-web-framework-and-became-an-open-source-maintainer/"
-            ),
+            "/blog/articles/2018/12/how-i-built-a-web-framework-and-became-an-open-source-maintainer/",  # noqa
         ),
         (
             "/consuming-apis-in-angular-displaying-data-in-components",
-            (
-                "/articles/2019/02"
-                "/consuming-apis-in-angular-displaying-data-in-components/"
-            ),
+            "/blog/articles/2019/02/consuming-apis-in-angular-displaying-data-in-components/",  # noqa
         ),
         (
             "/real-time-chatbot-server-python-bocadillo",
-            "/articles/2019/03/real-time-chatbot-server-python-bocadillo/",
+            "/blog/articles/2019/03/real-time-chatbot-server-python-bocadillo/",
         ),
         (
             "/attending-tech-conferences-beginner-guide",
-            "/articles/2019/05/attending-tech-conferences-beginner-guide/",
+            "/blog/articles/2019/05/attending-tech-conferences-beginner-guide/",
         ),
         (
             "/why-i-dont-write-on-medium",
-            "/articles/2019/05/why-i-dont-write-on-medium/",
+            "/blog/articles/2019/05/why-i-dont-write-on-medium/",
         ),
         (
             "/introducing-tartiflette-starlette",
-            "/articles/2019/07/introducing-tartiflette-starlette/",
+            "/blog/articles/2019/07/introducing-tartiflette-starlette/",
         ),
         (
             # Article was actually published in June.
             "/articles/2019/07/vuepress-upgrade-1-0",
-            "/articles/2019/06/vuepress-upgrade-1-0/",
+            "/blog/articles/2019/06/vuepress-upgrade-1-0/",
         ),
-        ("/vuepress-upgrade-1-0", "/articles/2019/06/vuepress-upgrade-1-0/"),
+        (
+            "/vuepress-upgrade-1-0",
+            "/blog/articles/2019/06/vuepress-upgrade-1-0/",
+        ),
         (
             "/introduction-to-asgi-async-python-web",
-            "/articles/2019/08/introduction-to-asgi-async-python-web/",
+            "/blog/articles/2019/08/introduction-to-asgi-async-python-web/",
         ),
         (
             "/python-optionally-parametrized-decorators",
-            "/articles/2019/09/python-optionally-parametrized-decorators/",
+            "/blog/articles/2019/09/python-optionally-parametrized-decorators/",
         ),
     ],
 )
 async def test_legacy_redirect_articles(
     client: httpx.AsyncClient,
-    blog_dot_dev_path: str,
-    dot_dev_path: str,
+    legacy_path: str,
+    path: str,
 ) -> None:
+    # blog.florimond.dev/xyz -> florimond.dev/blog/xyz
     resp = await client.get(
-        f"https://blog.florimond.dev{blog_dot_dev_path}", allow_redirects=True
+        f"https://blog.florimond.dev{legacy_path}", allow_redirects=True
     )
     assert resp.status_code == 200
-    assert resp.url == f"https://florimond.dev/blog{dot_dev_path}"
+    assert resp.url == f"https://florimond.dev{path}"
+
+    # No such redirection for florimond.dev/xyz (these URLs have never existed).
+    resp = await client.get(
+        f"https://florimond.dev{legacy_path}", allow_redirects=False
+    )
+    assert resp.status_code == 404
