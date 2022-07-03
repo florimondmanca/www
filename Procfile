f3b689394f631c0d0952e9729340d3baf7254c8f
florimondmanca <florimond.manca@protonmail.com>
2022-07-03 22:52:59 +0200

    Refactor: switch add main module

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn -b 0.0.0.0:${PORT} -w 1 -k uvicorn.workers.UvicornWorker server:app
+web: gunicorn -b 0.0.0.0:${PORT} -w 1 -k uvicorn.workers.UvicornWorker server.main:app
--- server/app.py
+++ server/app.py
@@ -1,14 +1,17 @@
 from starlette.applications import Starlette
+from starlette.types import ASGIApp
 
 from . import event_handlers, settings, views
 from .middleware import middleware
 from .routes import routes
 
-app = Starlette(
-    debug=settings.DEBUG,
-    routes=routes,
-    middleware=middleware,
-    exception_handlers={404: views.not_found, 500: views.internal_server_error},
-    on_startup=event_handlers.on_startup,
-    on_shutdown=event_handlers.on_shutdown,
-)
+
+def create_app() -> ASGIApp:
+    return Starlette(
+        debug=settings.DEBUG,
+        routes=routes,
+        middleware=middleware,
+        exception_handlers={404: views.not_found, 500: views.internal_server_error},
+        on_startup=event_handlers.on_startup,
+        on_shutdown=event_handlers.on_shutdown,
+    )
