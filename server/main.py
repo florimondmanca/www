aee41145df644ed3897f2aa39e4c5e71c089880b
Florimond Manca <florimond.manca@protonmail.com>
2022-11-11 17:57:37 +0100

    Switch to manual Ansible setup (#393)

--- server/main.py
+++ server/main.py
@@ -1,5 +1,6 @@
 import uvicorn
 
+from . import settings
 from .di import bootstrap
 from .web.app import create_app
 
@@ -8,4 +9,13 @@ bootstrap()
 app = create_app()
 
 if __name__ == "__main__":  # pragma: no cover
-    uvicorn.run("server.main:app", reload=True, use_colors=True)
+    config: dict = dict(
+        host=settings.HOST,
+        port=settings.PORT,
+        use_colors=True,
+    )
+
+    if settings.DEBUG:
+        config["reload"] = True
+
+    uvicorn.run("server.main:app", **config)
